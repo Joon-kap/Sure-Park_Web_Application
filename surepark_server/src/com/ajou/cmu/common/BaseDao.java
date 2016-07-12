01721e530da302d409be61f247d648980ae04f5d
jkpark <blamed@ajou.ac.kr>
2016-07-12 00:49:07 -0400

    branch test

--- surepark_server/src/com/ajou/cmu/common/BaseDao.java
+++ surepark_server/src/com/ajou/cmu/common/BaseDao.java
@@ -5,7 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
-
+/*branch test*/
 public class BaseDao extends SqlMapClientDaoSupport{
 	@Resource(name="sqlMapClient")
 	public void setDao(SqlMapClient sqlMapClient){
