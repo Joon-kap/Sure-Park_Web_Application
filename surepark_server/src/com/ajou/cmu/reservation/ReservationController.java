e4ea34af397826f802d852436f8ef822146eb773
Dae-Han Wyee <wdhwdh1@naver.com>
2016-07-18 06:47:15 +0900

    SetReservation함수 제거 및 Controller에서  Reservation.do 를 ServiceImple로 Mapping 까지 완료

--- surepark_server/src/com/ajou/cmu/reservation/ReservationController.java
+++ surepark_server/src/com/ajou/cmu/reservation/ReservationController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ajou.cmu.common.RequestParameter;
+import com.ajou.cmu.common.Service;
 import com.ajou.cmu.common.Utils;
 
 
@@ -89,7 +90,13 @@ public class ReservationController {
 		reservation.setpCreateDt(rp.get("ReservationCreateTime").toString());
 		reservation.setpUpdateDt(rp.get("ReservationUpdateTime").toString());
 		
+		revService.save(reservation);
 		
+		if(reservation !=null)	map.put("success", "Success to Registration");
+		else map.put("fail", "Fail to Registration");
+		
+		mv.addObject("map", map);
+		mv.addObject("callback", req.getParameter("callback"));
 			
 			return mv;
 	}
