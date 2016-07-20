de58f00258208631971a7e06477db98a6f4518f4
eunsang1cho <eunsang1cho@gmail.com>
2016-07-19 23:55:59 -0300

    WebSocket Retry add

--- surepark_server/src/com/ajou/cmu/reservation/ReservationController.java
+++ surepark_server/src/com/ajou/cmu/reservation/ReservationController.java
@@ -45,25 +45,8 @@ public class ReservationController {
 //		int num = Integer.parseInt((String) rp.get("testValue"));
 		String str = (String)rp.get("SENSORUPDATE");
 		
-//		System.out.println("num : "+ num);
-		/*
-		User user = (User) this.userService.getObject(rp);
+		//¿©±â¼­ ¼¾¼­¿¡¼­ ÀúÀåÇÏ°í ÀÖÀ» °ªÀ» str¿¡¼­ »Ì¾Æ³¿
 		
-		if(user != null){
-//			userMap.put("userSeq", user.getUserSeq());
-//			userMap.put("userEmail", user.getUserEmail());
-//			userMap.put("userName", user.getUserName());
-			
-			//ï¿½ê½­ï¿½ï¿½?ï¿½ï§£?ï¿½ï¿½?ï¿½ï¿½ - html5 sessionStorage ï¿½ì” ï¿½ìŠœ
-
-			map.put("1", user);
-		}else{
-			map.put("-1", "ï¿½ê¶—ï¿½ìŠœï¿½ì˜„ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ï¿½ì‚¤æ¹²ï¿½ ï¿½ë–Žï¿½ë™£");
-		}
-		mnv.addObject("map", map);
-		
-		mnv.addObject("callback", req.getParameter("callback"));
-		*/
 		
 		map.put("RETUPDATE", str);
 		mnv.addObject("map", map);
